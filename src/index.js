f8a6206d7a23891be461abcd8667ee77f7581ffd
[jmrtno] <[javiermartino96@gmail.com]>
2022-06-29 17:00:25 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './css/styles.css'
+
+import {Todo, TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach( crearTodoHtml );
+
+console.log('todos', todoList.todos);
